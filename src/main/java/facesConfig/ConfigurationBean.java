110066fb9683e05522a2d117c17abcf208ea6d9a
Tony Kovalenko <insane.z500@gmail.com>
2020-05-14 19:44:49 +0300

    Code cleanup.

--- src/main/java/facesConfig/ConfigurationBean.java
+++ src/main/java/facesConfig/ConfigurationBean.java
@@ -1,7 +1,8 @@
 package facesConfig;
 
-import static javax.faces.annotation.FacesConfig.Version.JSF_2_3;
 import javax.faces.annotation.FacesConfig;
+
+import static javax.faces.annotation.FacesConfig.Version.JSF_2_3;
 
 @FacesConfig(version = JSF_2_3)
 public class ConfigurationBean {
